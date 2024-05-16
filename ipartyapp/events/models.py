836d22dc82dc89f5c066890dea11d634624bbd2f
kahenyamercy <kahenyamercy5@gmail.com>
2024-05-16 11:22:34 +0300

    clean up git

--- ipartyapp/events/models.py
+++ ipartyapp/events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=100)
+    max_attendees = models.PositiveIntegerField()
+    contribution_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    campus = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
--- ipartyapp/events/views.py
+++ ipartyapp/events/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Event
 
-# Create your views here.
+class EventListView(ListView):
+    model = Event
+    template_name = 'event_list.html'
+    context_object_name = 'events'
+
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'event_detail.html'
+    context_object_name = 'event'
+
+class EventCreateView(LoginRequiredMixin, CreateView):
+    model = Event
+    template_name = 'event_form.html'
+    fields = ['name', 'date', 'time', 'location', 'max_attendees', 'contribution_amount', 'campus']
+
+class EventUpdateView(LoginRequiredMixin, UpdateView):
+    model = Event
+    template_name = 'event_form.html'
+    fields = ['name', 'date', 'time', 'location', 'max_attendees', 'contribution_amount', 'campus']
+
+class EventDeleteView(LoginRequiredMixin, DeleteView):
+    model = Event
+    template_name = 'event_confirm_delete.html'
+    success_url = '/events/'
