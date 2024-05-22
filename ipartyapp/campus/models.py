afa18292dbe75dd5c2a98ddba912309b5d0f421f
kahenyamercy <kahenyamercy5@gmail.com>
2024-05-22 04:16:01 +0300

    update the dependencies needed

--- ipartyapp/campus/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Campus(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    initials = models.CharField(max_length=10)
-    location = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
