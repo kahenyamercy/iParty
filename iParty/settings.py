dc0f133cb9a0c7e9760934efe1e0eb0b30262083
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-05-28 06:19:14 +0300

    Add landing page

--- iParty/settings.py
+++ iParty/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'campus',
     'bookings',
     'transactions',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'iParty.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
